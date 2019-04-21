e425335eb297173c0868f6bfaeaaf65fb079c9ac
chrishoen <chris123hoen@gmail.com>
2019-04-20 21:43:59 -0400

    added datagram to frontend2

--- frontend2/src/main.js
+++ frontend2/src/main.js
@@ -91,3 +91,26 @@ function startBackEnd() {
   console.log(`startBackEnd`);
 }
 
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind({
+  address: '127.0.0.1',
+  port: 56002,
+  exclusive: false
+});
