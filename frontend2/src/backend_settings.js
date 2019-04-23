134e01d8f45b1f25f9e5865f2f7a25c1829af33b
chrishoen <chris123hoen@gmail.com>
2019-04-23 19:01:36 -0400

    more

--- frontend2/src/backend_settings.js
+++ frontend2/src/backend_settings.js
@@ -3,11 +3,11 @@
 
 // Udp ip addresses and ports. The back end receives on
 // input ports and transmits on output ports.
-var mBackEndIpAddress  =  "127.0.0.1";    // backend  socket ip address.
-var mFrontEndIpAddress =  "127.0.0.1";    // frontend socket ip address.
-
 var mBackEndIpAddress  =  "192.168.1.9";  // backend  socket ip address.
 var mFrontEndIpAddress =  "192.168.1.3";  // frontend socket ip address.
+
+var mBackEndIpAddress  =  "127.0.0.1";    // backend  socket ip address.
+var mFrontEndIpAddress =  "127.0.0.1";    // frontend socket ip address.
 
 var mCommandInputPort  =  56001;       // The backend receives  on this port.
 var mCommandOutputPort =  56002;       // The backend transmits on this port.
