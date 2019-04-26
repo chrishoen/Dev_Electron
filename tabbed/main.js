4408ba14bbc48f2c5485c664d4f9c6196dd63945
chrishoen <chris123hoen@gmail.com>
2019-04-25 20:09:31 -0400

    more tabbed

--- tabbed/main.js
+++ tabbed/main.js
@@ -5,7 +5,8 @@ BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 440, x:0, y:0})
+//mainWindow = new BrowserWindow({width: 800, height: 440, x:0, y:0})
+  mainWindow = new BrowserWindow()
   mainWindow.loadURL('file://'+__dirname+'/index.html')
   mainWindow.on('closed', function () {
     console.log(`closed`);
--- tabbed/renderer.js
+++ tabbed/renderer.js
@@ -2,11 +2,48 @@ const path = require('path')
 const url = require('url')
 const ipc = require('electron').ipcRenderer;
 const TabGroup = require("electron-tabs");
+const dragula = require('dragula');
+
+const nodeConsole = require('console');
+const myconsole = new nodeConsole.Console(process.stdout, process.stderr);
 
 
-let tabGroup = new TabGroup();
-let tab = tabGroup.addTab({
-    title: "Electron",
-    src: "http://electron.atom.io",
-    visible: true
+myconsole.log('renderer start');
+
+
+var mTabGroup = new TabGroup({
+    ready: tabGroup => {
+        myconsole.log('mTab ready');
+    }
 });
+
+var mTab1 = mTabGroup.addTab({
+    title: 'Page1',
+    src: './page1.html',
+    webviewAttributes: {
+        'nodeintegration': true
+    },    
+    closable: false,
+    visible: true,
+    activate: true,
+    ready: tab => {
+      myconsole.log('mTab1 ready');
+    }
+});
+
+var mTab2 = mTabGroup.addTab({
+    title: 'Page2',
+    src: './page2.html',
+    webviewAttributes: {
+        'nodeintegration': true
+    },    
+    closable: false,
+    visible: true,
+    activate: false,
+    ready: tab => {
+      myconsole.log('mTab2 ready');
+    }
+});
+
+
+
