96846b7da8b25855fbb8912a0a76e90c76f43059
chrishoen <chris123hoen@gmail.com>
2019-04-28 13:41:39 -0400

    json experiments

--- frontend3/scratch.js
+++ frontend3/scratch.js
@@ -3,17 +3,36 @@
 
 console.log(`start**************************`);
 
-let tString1 = 'Command,Command1,arg0ccccccccccccccccccccccccccccccccccx';
-let tBuffer = Buffer.from(tString1);
+var tMessage1 = JSON.stringify({
+    'msgId' : 'msgType1',
+    'var1' : 'value1',
+});
 
-let tString2 = tBuffer.toString('utf8',0,20);
+console.log(`tMessage1       ${tMessage1}`);
+console.log(` `);
 
-let tTest = tBuffer.toString('utf8',0,20).startsWith('Command,');
+var tObj1 = JSON.parse(tMessage1);
 
-console.log(`tString1    ${tString1}`);
-console.log(`tBuffer     ${tBuffer}`);
-console.log(`tString2    ${tString2}`);
-console.log(`mTest       ${tTest}`);
+console.log(`tObj1.msgId     ${tObj1.msgId}`);
+console.log(`tObj1.var1      ${tObj1.var1}`);
+console.log(`tObj1.var2      ${tObj1.var2}`);
+console.log(` `);
+
+
+var tMessage2 = JSON.stringify({
+    'msgId' : 'msgType2',
+    'var1' : 'value1',
+    'var2' : 'value2',
+});
+
+console.log(`tMessage2       ${tMessage2}`);
+console.log(` `);
+
+var tObj2 = JSON.parse(tMessage2);
+
+console.log(`tObj2.msgId     ${tObj2.msgId}`);
+console.log(`tObj2.var1      ${tObj2.var1}`);
+console.log(`tObj2.var2      ${tObj2.var2}`);
 console.log(` `);
 
 
