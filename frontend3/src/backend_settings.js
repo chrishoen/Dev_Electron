8a237e3ce8f5ae1695cad84defa82fac35e36e29
chrishoen <chris123hoen@gmail.com>
2019-04-29 18:49:24 -0400

    settings for backend on tinkerboard

--- frontend3/src/backend_settings.js
+++ frontend3/src/backend_settings.js
@@ -2,8 +2,8 @@
 // BackEnd settings.
 
 // Udp ip addresses and ports.
-var mBackEndIpAddress  =  "192.168.1.9";  // backend  socket ip address.
 var mBackEndIpAddress  =  "127.0.0.1";    // backend  socket ip address.
+var mBackEndIpAddress  =  "192.168.1.9";  // backend  socket ip address.
 
 var mBackEndControlPort  =  56001;       // The backend  receives  on this port.
 var mFrontEndControlPort =  56002;       // The frontend receives on this port.
